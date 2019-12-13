d665b29c463d330feb5f289950fdb2a793c9b813
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-12-13 19:26:13 +0700

    Mengubah tampilan home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -30,8 +30,18 @@
             <div class="features-icons-icon d-flex">
               <i class="icon-screen-desktop m-auto text-primary"></i>
             </div>
-            <h3>Fully Responsive</h3>
-            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+            <figure class="figure">
+            <div class="figure-img">
+              <img src="<?= base_url();?>assets/img/model/2.png" class="figure-img img-fluid" width="150">
+              <a href="" class="d-flex justify-content-center">
+                <img src="img/detail.png" class="align-self-center">
+              </a>
+            </div>
+            <figcaption class="figure-caption text-center">
+              <h5>Baju Oblong</h5>
+              <p>IDR 100.000</p>
+            </figcaption>
+          </figure>
           </div>
         </div>
         <div class="col-lg-4">
@@ -39,8 +49,18 @@
             <div class="features-icons-icon d-flex">
               <i class="icon-layers m-auto text-primary"></i>
             </div>
-            <h3>Bootstrap 4 Ready</h3>
-            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+            <figure class="figure">
+            <div class="figure-img">
+              <img src="<?= base_url();?>assets/img/model/2.png" class="figure-img img-fluid" width="150">
+              <a href="" class="d-flex justify-content-center">
+                <img src="img/detail.png" class="align-self-center">
+              </a>
+            </div>
+            <figcaption class="figure-caption text-center">
+              <h5>Baju Keren</h5>
+              <p>IDR 120.000</p>
+            </figcaption>
+          </figure>
           </div>
         </div>
         <div class="col-lg-4">
@@ -48,8 +68,18 @@
             <div class="features-icons-icon d-flex">
               <i class="icon-check m-auto text-primary"></i>
             </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+            <figure class="figure">
+            <div class="figure-img">
+              <img src="<?= base_url();?>assets/img/model/2.png" class="figure-img img-fluid" width="150">
+              <a href="" class="d-flex justify-content-center">
+                <img src="img/detail.png" class="align-self-center">
+              </a>
+            </div>
+            <figcaption class="figure-caption text-center">
+              <h5>Baju Teu Keren</h5>
+              <p>IDR 10.000</p>
+            </figcaption>
+          </figure>
           </div>
         </div>
       </div>
