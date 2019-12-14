ea1eba373e091cbd7524014c098cec9837026205
adam <adamalfal8@mail.unpas.ac.id>
2019-12-15 00:52:42 +0700

    edit login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -14,7 +14,7 @@ class Auth extends CI_Controller{
         $this->form_validation->set_rules('password','Password', 'trim|required');
 
         if ($this->form_validation->run() == false) {
-            $data['title'] = 'WPU Login Page';
+            $data['title'] = 'Universe Login Admin';
             $this->load->view('template/auth_header', $data);
             $this->load->view('auth/login');
             $this->load->view('template/auth_footer');
@@ -34,15 +34,11 @@ class Auth extends CI_Controller{
             if ($user['is_active'] == 1) {
                 if (password_verify($password, $user['password'])) {
                     $data =[
-                        'email' => $user['email'],
-                        'role_id' => $user['role_id']
+                        'email' => $user['email']
                     ];
+
                     $this->session->set_userdata($data);
-                    if ($user['role_id'] == 1) {
-                        redirect('admin');
-                    }else{
-                        redirect('user');
-                    }
+                    redirect('admin');
                 }else{
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
                 redirect('auth');
@@ -56,40 +52,4 @@ class Auth extends CI_Controller{
             redirect('auth');
         }
     }
-
-    public function registration(){
-
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-            'is_unique' => "This email has already registered!"
-        ]);
-
-        $this->form_validation->set_rules('password','Password', 'required|trim|min_length[3]|matches[password2]',[
-            'matches' => 'Password dont match!',
-            'min_length'=>'Password to short!'
-        ]);
-
-        $this->form_validation->set_rules('password2' , 'Password2' , 'required|trim|matches[password]');
-
-        if ($this->form_validation->run() == false) {
-            $data['title'] = 'WPU User Registration';
-            $this->load->view('template/auth_header', $data);
-            $this->load->view('auth/registration');
-            $this->load->view('template/auth_footer');
-        }else{
-            $data = [
-                'name' => htmlspecialchars($this->input->post('name',true)),
-                'email' => htmlspecialchars($this->input->post('email',true)),
-                'image' => 'default.png',
-                'password'=>password_hash(htmlspecialchars($this->input->post('password')),PASSWORD_DEFAULT),
-                'role_id' => 2,
-                'is_active' => 1,
-                'date_created'=>time(),
-            ];
-            $this->db->insert('user',$data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please Login</div>');
-            redirect('auth');
-
-    }
-} 
 }
